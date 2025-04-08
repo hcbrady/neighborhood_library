59b3727b8a1e76138e1e4fe9f16369a6c881ab32
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-04-07 21:38:19 -0400

    geocode addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,10 @@
 
 class Address < ApplicationRecord
   has_many :users
+  geocoded_by :full_address_for_geocode
+  after_validation :geocode
+
+  def full_address_for_geocode
+    "#{street1}, #{city}, #{state} #{zipcode}"
+  end
 end
