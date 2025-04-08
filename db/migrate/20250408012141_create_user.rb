0756dd8f98949c58786117dfa43471252427fbe8
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-04-07 21:26:46 -0400

    database and associations

---
+++ db/migrate/20250408012141_create_user.rb
@@ -0,0 +1,12 @@
+class CreateUser < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :address_id
+
+      t.timestamps
+    end
+  end
+end
