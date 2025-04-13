74260ae6e81686f80374c0cf1ede58a3b8cf096a
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-04-13 18:14:09 -0400

    ability to update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,25 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user, notice: "User was successfully updated."
+    else
+      redirect_to @user, alert: "User failed to update."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(
+            :first_name,
+            :last_name,
+            :email
+          )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "dashboard#index"
 
-  resources :users, only: %i[show]
+  resources :users, only: %i[show update]
 end
