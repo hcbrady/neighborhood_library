14224e451f068e7fd54e64d122058f53d12d7412
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-04-13 16:45:12 -0400

    full address on address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,4 +8,22 @@ class Address < ApplicationRecord
   def full_address_for_geocode
     "#{street1}, #{city}, #{state} #{zipcode}"
   end
+
+  def full_address
+    if street2.present? && street1.present? && city.present? && state.present?
+      "#{street1}<br />#{street2}<br />#{city}, #{state} #{zipcode}".html_safe
+    elsif street1.present? && city.present? && state.present?
+      "#{street1}<br />#{city}, #{state} #{zipcode}".html_safe
+    elsif city.present? && state.present?
+      "#{city}, #{state} #{zipcode}"
+    elsif city.present?
+      "#{city} #{zipcode}"
+    elsif state.present?
+      "#{state} #{zipcode}"
+    elsif zip.present?
+      zip
+    else
+      "N/A"
+    end
+  end
 end
