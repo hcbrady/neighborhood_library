965c58ea116d236a021c1017ed7f0f5ade292876
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-05-04 18:51:55 -0400

    added genre options to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,26 @@
 
 class Book < ApplicationRecord
   belongs_to :user
+
+  def self.genres
+    [
+      "Fantasy",
+      "Science Fiction",
+      "Horror",
+      "Fiction",
+      "Memoir",
+      "Non-Fiction",
+      "Historical Fiction",
+      "Biography",
+      "Classic",
+      "Short Story",
+      "Poetry",
+      "Cook Book",
+      "Art Book",
+      "Mystery",
+      "Thriller",
+      "Children",
+      "Young Adult"
+    ]
+  end
 end
